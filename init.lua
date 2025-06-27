ca3955a36a7a9b99955219350be47ba4fbcb4be0
Vladimir Kovac <vkovac@amazon.com>
2025-06-27 20:08:24 +0000

    add compile command

--- init.lua
+++ init.lua
@@ -189,6 +189,31 @@ require('lazy').setup({
   { 'nvim-treesitter/nvim-treesitter-context', opts = { multiline_threshold = 1 } },
   { 'tpope/vim-endwise', ft = { 'ruby', 'eruby' } },
   {
+    'ej-shafran/compile-mode.nvim',
+    version = '^5.0.0',
+    -- you can just use the latest version:
+    -- branch = "latest",
+    -- or the most up-to-date updates:
+    -- branch = "nightly",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- if you want to enable coloring of ANSI escape codes in
+      -- compilation output, add:
+      { "m00qek/baleia.nvim", tag = "v1.3.0" },
+    },
+    config = function()
+      ---@type CompileModeOpts
+      vim.g.compile_mode = {
+        -- to add ANSI escape code support, add:
+        baleia_setup = true,
+
+        -- to make `:Compile` replace special characters (e.g. `%`) in
+        -- the command (and behave more like `:!`), add:
+        -- bang_expansion = true,
+      }
+    end,
+  },
+  {
     'stevearc/oil.nvim',
     opts = {},
     -- Optional dependencies
@@ -547,6 +572,7 @@ require('lazy').setup({
         jdtls = {},
         smithy_ls = {
         },
+        ts_ls = {},
 
         -- ruby_lsp = {
         --   root_dir = require('lspconfig.util').root_pattern('.env', 'Gemfile', '.git'),
